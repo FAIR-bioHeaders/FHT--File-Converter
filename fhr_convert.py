5f15755cc7109761de3acc5b9320700bf4011b91
Adam Wright <adam.wright@wormbase.org>
2024-03-25 15:18:29 -0400

    Update fhr_convert.py

--- fhr_convert.py
+++ fhr_convert.py
@@ -2,19 +2,19 @@ import argparse
 import sys
 import textwrap
 
-from fhr import fhr
+from fht import fht
 
 
 def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
-        description="Convert from one FHR supported file type to another",
+        description="Convert from one FHT supported file type to another",
         epilog=textwrap.dedent(
             """\
                     positional <file> input and output files
                         input files can be one of:
                             <input>.yml
-                            <input>.fasta  - fasta contining a fhr header
+                            <input>.fasta  - fasta contining a fht header
                             <input>.html   - html containing microdata
                             <input>.json
                             <input>.gfa
@@ -37,19 +37,19 @@ def main():
 
     args = parser.parse_args()
 
-    fhr_to_be_converted = fhr()
+    fht_to_be_converted = fht()
 
     with open(args.file[0], "r") as input_file:
         if args.file[0].endswith(".yml") or args.file[0].endswith(".yaml"):
-            fhr_to_be_converted.input_yaml(input_file)
+            fht_to_be_converted.input_yaml(input_file)
         elif args.file[0].endswith(".fasta") or args.file[0].endswith(".fa"):
-            fhr_to_be_converted.input_fasta(input_file)
+            fht_to_be_converted.input_fasta(input_file)
         elif args.file[0].endswith(".json"):
-            fhr_to_be_converted.input_json(input_file)
+            fht_to_be_converted.input_json(input_file)
         elif args.file[0].endswith(".html"):
-            fhr_to_be_converted.input_microdata(input_file)
+            fht_to_be_converted.input_microdata(input_file)
         elif args.file[0].endswith(".gfa"):
-            fhr_to_be_converted.input_gfa(input_file)
+            fht_to_be_converted.input_gfa(input_file)
         else:
             sys.exit("Input file extention not found")
 
@@ -58,15 +58,15 @@ def main():
     with open(args.file[1], "w") as output_file:
         sys.stdout = output_file
         if args.file[1].endswith(".yml") or args.file[1].endswith(".yaml"):
-            print(fhr_to_be_converted.ouput_yaml())
+            print(fht_to_be_converted.ouput_yaml())
         elif args.file[1].endswith(".fasta") or args.file[1].endswith(".fa"):
-            print(fhr_to_be_converted.output_fasta())
+            print(fht_to_be_converted.output_fasta())
         elif args.file[1].endswith(".json"):
-            print(fhr_to_be_converted.output_json())
+            print(fht_to_be_converted.output_json())
         elif args.file[1].endswith(".html"):
-            print(fhr_to_be_converted.output_microdata())
+            print(fht_to_be_converted.output_microdata())
         elif args.file[1].endswith(".gfa"):
-            print(fhr_to_be_converted.output_gfa())
+            print(fht_to_be_converted.output_gfa())
         else:
             sys.stdout = original_stdout
             sys.exit("Output file extention not found")
