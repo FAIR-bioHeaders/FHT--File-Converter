9fb99b647d1786cd36282667e40e264b315ad8fa
Adam Wright <adam.wright@wormbase.org>
2024-03-25 15:14:18 -0400

    Update fhr_validate.py

--- fhr_validate.py
+++ fhr_validate.py
@@ -2,22 +2,22 @@ import argparse
 import sys
 import textwrap
 
-from fhr import fhr
+from fht import fht
 
 
 def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
-        description="Validate a FHR metadata file",
+        description="Validate a FHT metadata file",
         epilog=textwrap.dedent(
             """\
                     positional <file> input and output files
                         input files can be one of:
                             <input>.yml
-                            <input>.fasta  - fasta contining a fhr header
+                            <input>.fasta  - fasta contining a fht header
                             <input>.html   - html containing microdata
-                            <input>.json   - json containing a fhr header
-                            <input>.gfa    - gfa file contining a fhr header
+                            <input>.json   - json containing a fht header
+                            <input>.gfa    - gfa file contining a fht header
                             """
         ),
     )
@@ -30,20 +30,20 @@ def main():
 
     args = parser.parse_args()
 
-    fhr_to_be_validated = fhr()
+    fht_to_be_validated = fht()
 
     with open(args.file[0], "r") as input_file:
         if args.file[0].endswith(".yml") or args.file[0].endswith(".yaml"):
-            fhr_to_be_validated.input_yaml(input_file)
+            fht_to_be_validated.input_yaml(input_file)
         elif args.file[0].endswith(".fasta") or args.file[0].endswith(".fa"):
-            fhr_to_be_validated.input_fasta(input_file)
+            fht_to_be_validated.input_fasta(input_file)
         elif args.file[0].endswith(".json"):
-            fhr_to_be_validated.input_json(input_file)
+            fht_to_be_validated.input_json(input_file)
         elif args.file[0].endswith(".html"):
-            fhr_to_be_validated.input_microdata(input_file)
+            fht_to_be_validated.input_microdata(input_file)
         elif args.file[0].endswith(".gfa"):
-            fhr_to_be_validated.input_gfa(input_file)
+            fht_to_be_validated.input_gfa(input_file)
         else:
             sys.exit("Input file extention not found")
 
-    fhr_to_be_validated.fhr_validate()
+    fht_to_be_validated.fht_validate()
