13113ff9e1f6873c6c752d9f2f63c09b540068d1
Adam Wright <adam.wright@wormbase.org>
2024-03-25 15:21:26 -0400

    Update fhr_fasta_combine.py

--- fasta/fhr_fasta_combine.py
+++ fasta/fhr_fasta_combine.py
@@ -3,19 +3,19 @@ import os
 import sys
 import textwrap
 
-from fhr import fhr
+from fht import fht
 
 
 def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
-        description="Validate a FHR metadata file",
+        description="Validate a FHT metadata file",
         epilog=textwrap.dedent(
             """\
                     positional <file> input and output files
                         input files can be one of:
                             <input>.yml
-                            <input>.fasta  - fasta contining a fhr header
+                            <input>.fasta  - fasta contining a fht header
                             <input>.html   - html containing microdata
                             <input>.json
                             <input>.gfa
@@ -33,27 +33,27 @@ def main():
 
     args = parser.parse_args()
 
-    fhr_to_be_combined = fhr()
+    fht_to_be_combined = fht()
 
     with open(args.file[0], "r") as input_file:
         if args.file[0].endswith(".yml") or args.file[0].endswith(".yaml"):
-            fhr_to_be_combined.input_yaml(input_file)
+            fht_to_be_combined.input_yaml(input_file)
         elif args.file[0].endswith(".fasta") or args.file[0].endswith(".fa"):
-            fhr_to_be_combined.input_fasta(input_file)
+            fht_to_be_combined.input_fasta(input_file)
         elif args.file[0].endswith(".json"):
-            fhr_to_be_combined.input_json(input_file)
+            fht_to_be_combined.input_json(input_file)
         elif args.file[0].endswith(".html"):
-            fhr_to_be_combined.input_microdata(input_file)
+            fht_to_be_combined.input_microdata(input_file)
         else:
             sys.exit("Input file extention not found")
 
-    output_filename = os.path.splitext(args.file[1])[0] + ".fhr.fasta"
+    output_filename = os.path.splitext(args.file[1])[0] + ".fht.fasta"
 
     with open(args.file[0], "r") as sources:
         fasta_lines = sources.readlines()
 
     with open(output_filename, "w") as output_file:
-        output_content = fhr_to_be_combined.output_fasta()
+        output_content = fht_to_be_combined.output_fasta()
         output_file.write(output_content)
         for line in fasta_lines:
             output_file.write(line)
